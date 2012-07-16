528f06d1999e67ff7b8e05f858ab7a713693e37e
gregxross <gregxross@gmail.com>
2012-07-16 07:03:06 +0000

    Enabled mousewheel scrolling.

--- js/VisiScroll.js
+++ js/VisiScroll.js
@@ -1,13 +1,59 @@
 VisiScroll = function(htmlContent){
 
+    this.hookEvent = function(element, eventName, callback){
+        if (typeof(element) == "string") 
+            element = document.getElementById(element);
+        if (element == null) 
+            return;
+        if (element.addEventListener) {
+            if (eventName == 'mousewheel') 
+                element.addEventListener('DOMMouseScroll', callback, false);
+            element.addEventListener(eventName, callback, false);
+        }
+        else 
+            if (element.attachEvent) 
+                element.attachEvent("on" + eventName, callback);
+    };
+    
+    this.cancelEvent = function(e){
+        e = e ? e : window.event;
+        if (e.stopPropagation) 
+            e.stopPropagation();
+        if (e.preventDefault) 
+            e.preventDefault();
+        e.cancelBubble = true;
+        e.cancel = true;
+        e.returnValue = false;
+        return false;
+    };
+	
+	var self = this;
+	
+	this.mouseWheelCallback = function(e){
+		
+		var normal = e.detail ? e.detail * -1 : e.wheelDelta / 40;
+		console.log(normal);
+		
+		self.sliderClicked = true;
+		
+		var newPos = self.slidingWindow.y + normal * -1;
+        self.positionSlider(newPos);
+		
+		self.sliderClicked = false;
+		
+		self.cancelEvent(e);
+	};
+	
+	this.hookEvent(htmlContent, 'mousewheel', this.mouseWheelCallback);
+    
     SlidingWindow = function(slider){
         var self = this;
         this.y = 0;
         this.height = (contentHeight / $(htmlContent).height()) * contentHeight;
-        this.lastSetPosition = 0
+        this.lastSetPosition = 0;
         
         this.redraw = function(scroll){
-            
+        
             var lingrad = slider.canvasContext.createLinearGradient(0, 0, slider.canvas.width, 0);
             lingrad.addColorStop(0, 'rgba(204, 204, 204, 0.65)');
             lingrad.addColorStop(0.5, 'rgba(255, 255, 255, 0.65)');
@@ -29,8 +75,6 @@ VisiScroll = function(htmlContent){
         
         this.redraw(false);
     };
-	
-	var self = this;
     
     this.mouseClickedInSlider = function(y){
         return y > self.slidingWindow.y && y < (self.slidingWindow.y + self.slidingWindow.height);
@@ -54,9 +98,9 @@ VisiScroll = function(htmlContent){
     
     this.canvas = document.createElement("canvas");
     
-    this.positionSlider = function(newPos) {
+    this.positionSlider = function(newPos){
         if (self.sliderClicked) {
-            
+        
             if (newPos < 0) 
                 self.slidingWindow.y = 0;
             else 
@@ -80,7 +124,7 @@ VisiScroll = function(htmlContent){
             self.slidingWindow.lastClickedPos = clickPosY;
         }
         else {
-            self.positionSlider(clickPosY-10);
+            self.positionSlider(clickPosY - 10);
         }
     });
     
@@ -119,15 +163,15 @@ VisiScroll = function(htmlContent){
         self.canvasContext.fillRect(0, 0, self.canvas.width, self.canvas.height);
     };
     
-    this.renderMarginMarks = function(recalculateMarkers) {
+    this.renderMarginMarks = function(recalculateMarkers){
         var sliderMarkerElements = $(htmlContent).find('[visiScrollMarker]');
         
         if (recalculateMarkers || !this.markers) {
             this.markers = sliderMarkerElements.map(function(){
-				
-				var top = this.offsetTop - $(this).parent().position().top;
-				top = top < 0 ? this.offsetTop : top;
-				
+            
+                var top = this.offsetTop - $(this).parent().position().top;
+                top = top < 0 ? this.offsetTop : top;
+                
                 return {
                     top: top,
                     colour: this.attributes.visiScrollMarker.nodeValue,
@@ -145,7 +189,7 @@ VisiScroll = function(htmlContent){
             
             self.canvasContext.fillStyle = lingrad;
             var tTop = (self.canvas.height / $(htmlContent).height()) * marker.top;
-            self.canvasContext.fillRect(1, tTop, self.canvas.width-1, marker.height < 2 ? 2 : marker.height);
+            self.canvasContext.fillRect(1, tTop, self.canvas.width - 1, marker.height < 2 ? 2 : marker.height);
         });
     }
     
